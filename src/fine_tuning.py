f4777bedbae5962c2d23d76906ac1fc2a4611b20
Elke Kohlmann <kohlmane@gruenau3.informatik.hu-berlin.de>
2024-03-09 18:35:37 +0100

    HF playing

--- src/fine_tuning.py
+++ src/fine_tuning.py
@@ -1,5 +1,11 @@
 import torch
-from transformers import AutoTokenizer, T5ForConditionalGeneration, AutoModelForSeq2SeqLM, AutoModelForSequenceClassification 
+from transformers import (
+                        AutoTokenizer,
+                        T5ForConditionalGeneration,
+                        AutoModelForSeq2SeqLM,
+                        AutoModelForSequenceClassification,
+                        AdamW
+                        )
 from datasets import load_dataset
 from datasets import load_dataset_builder
 import numpy as np
@@ -61,20 +67,64 @@ print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 start_time = time.time()
 i = 1
 for snippet in train_dataset['code']:
-    print("\n############################################")
-    print("i: ", i)  # num_rows: 2770 == i
-    print("snippet: ", snippet)       
-    inputs = tokenizer.encode(snippet, return_tensors="pt").to(device)
-    #print("inputs: ", inputs)
-    outputs = model.generate(inputs)
-    i += 1
+    if i < 3:   # nur zum testen
+        print("\n############################################")
+        print("i: ", i)  # num_rows: 2770 == i
+        print("snippet: ", snippet)
+
+        # aus tutorial HF nlp/2/5: keine Ahnung, warum ich das brauche ...
+        # ich habe ja unten schon auch tensors, mit return tensors ...
+        # klappt auch nicht
+        #tokens = tokenizer.tokenize(snippet)
+        #print("tokens: ", tokens)
+        #ids = tokenizer.convert_tokens_to_ids(tokens)
+        #print("ids: ", ids)
+        #input_ids = torch.tensor([ids])
+        #print("input_ids: ", input_ids)
+        #output = model(input_ids)
+        #print("Logits: ", output.logits)
+        ########################################
+
+
+
+        inputs = tokenizer.encode(snippet, return_tensors="pt").to(device)
+        print("inputs: ", inputs)       
+        outputs = model.generate(model_inputs)
+
+        # UserWarning: Using the model-agnostic default `max_length` (=20) to control the generation length.
+        # We recommend setting `max_new_tokens` to control the maximum length of the generation.
+
+
+
+        # hf_outputs = model(**inputs)  # Nice try: hier wollte ich dieselbe Ausgabe machen wie im HF Turorial
+        # ist aber trotzdem nicht unspannend, weil ich hier trotz Fehlermeldung Infos bekomme
+        #print("outputs.last_hidden_state.shape: ", outputs.last_hidden_state.shape)
+        #AttributeError: 'Tensor' object has no attribute 'last_hidden_state'
+        #print("outputs.logit.shape: ", outputs.logits.shape)
+        #AttributeError: 'builtin_function_or_method' object has no attribute 'shape'
+        #print("outputs.logit: ", outputs.logits)
+        #Output: outputs.logit:  <built-in method logits of Tensor object at 0x7f8929c6d730>
+        #predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+        #AttributeError: 'builtin_function_or_method' object has no attribute 'softmax'
+    
+        i += 1
+
+
+#print("outputs.last_hidden_state.shape: ", outputs.last_hidden_state.shape)
+#AttributeError: 'Tensor' object has no attribute 'last_hidden_state'
+#print("outputs.logits.shape: ", outputs.logits.shape)
+##AttributeError: 'Tensor' object has no attribute 'logits'. Did you mean: 'logit'?
+#print("outputs.logits: ", outputs.logits)
+#predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+#print("predictions: ", predictions)
+#print("labels: ", model.config.id2label)
 
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print("elapsed_time: ", time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
 
-
+#train a sequence classifier on one batch in PyTorch:
 
 
 
