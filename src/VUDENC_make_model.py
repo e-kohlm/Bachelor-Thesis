5e129742acc357b15e90f334871ebe81bf4bdfb4
Elke Kohlmann <kohlmane@gruenau3.informatik.hu-berlin.de>
2024-03-09 23:08:02 +0100

    Fine_tuning draft  #1

--- src/VUDENC_make_model.py
+++ src/VUDENC_make_model.py
@@ -125,6 +125,7 @@ with open('test_allblocks.json', 'w') as f:
 ############## meins######
 ##### allblocks [] ist der komplett code nach filtering
 
+#######################################WORKING BUT NOT NECESSARY#########################
 
 keys = []
 
@@ -184,9 +185,21 @@ print("time: ", nowformat)
 ######################################################
 
 # saving samples
-with open('../VUDENC_data/' + 'EXAMPLE_' + mode + '_dataset-TRAINING', 'w') as fp:
+"""with open('../VUDENC_data/' + 'EXAMPLE_' + mode + '_dataset-TRAINING', 'w') as fp:
+    fp.write(json.dumps(training_set))"""
+
+# Nur für jetzt, damit ich selbe Länge habe
+with open('../VUDENC_data/' + 'EXAMPLE_' + mode + '_dataset', 'w') as fp:
     fp.write(json.dumps(training_set))
-with open('../VUDENC_data/' + 'EXAMPLE_' + mode + '_dataset-VALIDATION', 'w') as fp:
+
+"""with open('../VUDENC_data/' + 'EXAMPLE_' + mode + '_dataset-VALIDATION', 'w') as fp:
     fp.write(json.dumps(validation_set))
 with open('../VUDENC_data/' + 'EXAMPLE_' + mode + '_dataset-TESTING', 'w') as fp:
-    fp.write(json.dumps(test_set))
+    fp.write(json.dumps(test_set))"""
+
+######## BIS hier für splitting daten
+
+
+"""# ohne splitting geht  das dann wohl einfach so: 
+with open('../VUDENC_data/' + 'EXAMPLE_' + mode + '_dataset', 'w') as fp:
+    fp.write(json.dumps(allblocks))"""
--- src/fine_tuning.py
+++ src/fine_tuning.py
@@ -4,26 +4,32 @@ from transformers import (
                         T5ForConditionalGeneration,
                         AutoModelForSeq2SeqLM,
                         AutoModelForSequenceClassification,
-                        AdamW
+                        AdamW,
+                        TrainingArguments,
+                        Trainer,
+                        DataCollatorWithPadding
                         )
 from datasets import load_dataset
 from datasets import load_dataset_builder
 import numpy as np
 import codecs
 from contextlib import redirect_stdout
+from torch.utils.data import DataLoader
 import time
 
 
 
 file_path = "../VUDENC_data/"
-training_set = 'EXAMPLE_sql_dataset-TRAINING'
-
+#training_set = "EXAMPLE_sql_dataset-TRAINING"
+#validation_set = "EXAMPLE_sql_dataset-VALIDATION"
+raw_datasets = "EXAMPLE_sql_dataset"
+"""
 # Load data and get some information about it
 train_dataset = load_dataset("json", data_files=file_path + training_set, split='train')
 print(train_dataset)
 print(train_dataset.features)
 print(train_dataset[0])
-print(train_dataset[-1])
+print(train_dataset[-1])"""
 
 # Working: from Hugging Face Tutorial
 """checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"  # TODO: checkpoint = weights, which one?
@@ -45,12 +51,12 @@ end_time = time.time()
 elapsed_time = end_time - start_time
 print("elapsed_time: ", time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))"""
 
-# CodeT5
+"""# CodeT5
 checkpoint = "Salesforce/codet5p-220m"
 device = "cpu" # "cuda" for GPU usage or "cpu" for CPU usage
 
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = T5ForConditionalGeneration.from_pretrained(checkpoint).to(device)
+model = T5ForConditionalGeneration.from_pretrained(checkpoint).to(device)"""
 
 # Code T5 from extra code on the right: TODO: welches soll ich nutzen???? wo finde ich solche Infos?
 # Load model directly
@@ -64,13 +70,12 @@ outputs = model.generate(inputs, max_length=10)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 # ==> print "Hello World"""
 
-start_time = time.time()
-i = 1
+"""i = 1
 for snippet in train_dataset['code']:
     if i < 3:   # nur zum testen
         print("\n############################################")
         print("i: ", i)  # num_rows: 2770 == i
-        print("snippet: ", snippet)
+        print("snippet: ", snippet)"""
 
         # aus tutorial HF nlp/2/5: keine Ahnung, warum ich das brauche ...
         # ich habe ja unten schon auch tensors, mit return tensors ...
@@ -85,16 +90,12 @@ for snippet in train_dataset['code']:
         #print("Logits: ", output.logits)
         ########################################
 
-
-
-        inputs = tokenizer.encode(snippet, return_tensors="pt").to(device)
-        print("inputs: ", inputs)       
-        outputs = model.generate(model_inputs)
+        #inputs = tokenizer.encode(snippet, return_tensors="pt").to(device)
+        #print("inputs: ", inputs)       
+        #outputs = model.generate(model_inputs)
 
         # UserWarning: Using the model-agnostic default `max_length` (=20) to control the generation length.
         # We recommend setting `max_new_tokens` to control the maximum length of the generation.
-
-
 
         # hf_outputs = model(**inputs)  # Nice try: hier wollte ich dieselbe Ausgabe machen wie im HF Turorial
         # ist aber trotzdem nicht unspannend, weil ich hier trotz Fehlermeldung Infos bekomme
@@ -106,9 +107,8 @@ for snippet in train_dataset['code']:
         #Output: outputs.logit:  <built-in method logits of Tensor object at 0x7f8929c6d730>
         #predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
         #AttributeError: 'builtin_function_or_method' object has no attribute 'softmax'
-    
-        i += 1
 
+        # i += 1
 
 #print("outputs.last_hidden_state.shape: ", outputs.last_hidden_state.shape)
 #AttributeError: 'Tensor' object has no attribute 'last_hidden_state'
@@ -119,12 +119,99 @@ for snippet in train_dataset['code']:
 #print("predictions: ", predictions)
 #print("labels: ", model.config.id2label)
 
+###########################################################
+
+
+# Train a sequence classifier on one batch in PyTorch:
+# Same as before
+#checkpoint = "bert-base-uncased"
+#tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+#model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+
+# CodeT5
+
+dataset = load_dataset(path="json", data_files=file_path + raw_datasets)
+checkpoint = "Salesforce/codet5p-220m"
+device = "cpu" # "cuda" for GPU usage or "cpu" for CPU usage
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, num_labels=2)
+#model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+model = T5ForConditionalGeneration.from_pretrained(checkpoint).to(device)
+
+def tokenize_function(raw_datasets):
+    print("tokenizer: ", tokenizer)
+    return tokenizer(raw_datasets['code'], truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+print("tokenized_datasets: ", tokenized_datasets)
+tokenized_datasets = tokenized_datasets.remove_columns(["snippet_id"])
+tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+tokenized_datasets.set_format("torch")
+tokenized_datasets["train"].column_names
+print("AGAIN tokenized_datasets: ", tokenized_datasets)
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+training_args = TrainingArguments("test-trainer")
+
+
+train_dataloader = DataLoader(
+    tokenized_datasets["train"], shuffle=True, batch_size=8, collate_fn=data_collator
+)
+eval_dataloader = DataLoader(
+    tokenized_datasets["validation"], batch_size=8, collate_fn=data_collator
+)
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],  # findet der nicht, warum findet er train_dataset?
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+start_time = time.time()
+
+#batch = tokenizer(snippet, padding=True, truncation=True, return_tensors="pt")
+
+# This is new
+#batch["labels"] = torch.tensor([0, 1])
+
+#optimizer = torch.optim.AdamW(model.parameters())
+#loss = model(**batch).loss
+#loss.backward()
+#optimizer.step()
+
+
+
+
+
+
+
+
+trainer.train()
+
 
 end_time = time.time()
-elapsed_time = end_time - start_time
-print("elapsed_time: ", time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+time_elapsed = end_time - start_time
+print("time_elapsed: ", time.strftime("%H:%M:%S", time.gmtime(time_elapsed)))        
 
-#train a sequence classifier on one batch in PyTorch:
+        
+
+
+
+
+
+
+
+
+
+sequences = [
+    "I've been waiting for a HuggingFace course my whole life.",
+    "This course is amazing!",
+]
+
 
 
 
