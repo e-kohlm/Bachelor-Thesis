c1f2f871448d7029bf5b2009f8da77c7cdb93535
Elke Kohlmann <kohlmane@gruenau3.informatik.hu-berlin.de>
2024-03-10 22:27:13 +0100

    HF trainer trains something

--- src/fine_tuning.py
+++ src/fine_tuning.py
@@ -21,8 +21,9 @@ import time
 
 
 file_path = "../VUDENC_data/"
-#training_set = "EXAMPLE_sql_dataset-TRAINING"
-#validation_set = "EXAMPLE_sql_dataset-VALIDATION"
+training_set = "EXAMPLE_sql_dataset-TRAINING"
+validation_set = "EXAMPLE_sql_dataset-VALIDATION"
+test_set = "EXAMPLE_sql_dataset-TESTING"
 raw_datasets = "EXAMPLE_sql_dataset"
 """
 # Load data and get some information about it
@@ -129,45 +130,76 @@ for snippet in train_dataset['code']:
 #tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 #model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
 
-# CodeT5
 
-datasets = load_dataset(path="json", data_files=file_path + raw_datasets)
+
+
+# CodeT5
+data_files = {"train": file_path + training_set, "validation": file_path + validation_set, "test": file_path + test_set}
+datasets = load_dataset("json", data_files=data_files)
+print("datasets: ", datasets)
+
+#checkpoint = "bert-base-uncased"
 checkpoint = "Salesforce/codet5p-220m"
 device = "cpu" # "cuda" for GPU usage or "cpu" for CPU usage
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, num_labels=2)
-model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+#model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 #model = T5ForConditionalGeneration.from_pretrained(checkpoint).to(device)
 
+"""i = 1
+for snippet in datasets:
+    
+    print("\n############################################")
+    print("i: ", i)  # num_rows: 2770 == i
+    #print("snippet: ", snippet['code'])
+    
+    # train a sequence classifier
+    batch = tokenizer(snippet['code'], padding=True, truncation=True, return_tensors="pt")
+    #print("batch: ", batch)
 
-# train a sequence classifier
-batch = tokenizer(datasets, padding=True, truncation=True, return_tensors="pt")
+    # This is new    
+    #batch["labels"] = torch.tensor([1,1] )  # TODO: Was sind das für labels? Ich denke schon, wie erwartet, je 1 für sentiment
+    #ValueError: Expected input batch_size (1) to match target batch_size (2).
+    #Der kommt genau von batch[labels]: hier hat er 2 label, denn 2 Sätze im HF example
+    #Ich hingegeben, geb im Loop immer nur 1 Satz
+    
+    batch['labels'] = torch.tensor(snippet['label'])
+    #print("batch['labels']: ", batch['labels'])
+    optimizer = torch.optim.AdamW(model.parameters())
+    #print("optimizer: ", optimizer)
+    loss = model(**batch).loss
+    #print("loss: ", loss)
+    loss_bw = loss.backward()
+    #print("loss.backward: ", loss_bw)
+    opt_step = optimizer.step()
+    #print("optimizer.step: ", opt_step)
+    i += 1   
+    # Works not with T5 model: IndexError: too many indices for tensor of dimension 0 , auch nicht mit num_labels = 2
+    # It is also not the best solution according to HF cahpter 3/2"""
 
-# This is new
-batch["labels"] = torch.tensor([1, 1])
 
-optimizer = torch.optim.AdamW(model.parameters())
-loss = model(**batch).loss
-loss.backward()
-optimizer.step()
+
+
+def tokenize_function(datasets):    
+    return tokenizer(datasets['code'], truncation=True) 
 
 
 
 
-def tokenize_function(datasets):
-    print("tokenizer: ", tokenizer)
-    return tokenizer(datasets['code'], truncation=True)
+
 
 tokenized_datasets = datasets.map(tokenize_function, batched=True)
 print("tokenized_datasets: ", tokenized_datasets)
 tokenized_datasets = tokenized_datasets.remove_columns(["snippet_id"])
 tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
 tokenized_datasets.set_format("torch")
-tokenized_datasets["train"].column_names
+tokenized_datasets.column_names
 print("AGAIN tokenized_datasets: ", tokenized_datasets)
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+#print("data_collator: ", data_collator)
 # test
-samples = tokenized_datasets["train"][:8]
+"""samples = tokenized_datasets["train"][:8]
 samples = {k: v for k, v in samples.items() if k not in ["idx", "sentence1", "sentence2"]}
 [len(x) for x in samples["input_ids"]]
 print("samples: ", samples)
@@ -175,10 +207,12 @@ print("samples: ", samples)
 
 batch = data_collator(samples)
 {k: v.shape for k, v in batch.items()}
-print("batch: ", batch)
+print("batch: ", batch)"""
 
 ###########
 
+# Checkpoint destination directory test-trainer/checkpoint-500 already exists and is non-empty.
+# Saving will proceed but saved results may be invalid.
 training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
 
 
@@ -190,40 +224,54 @@ train_dataloader = DataLoader(
 eval_dataloader = DataLoader(
     tokenized_datasets["validation"], batch_size=8, collate_fn=data_collator
 )
+print("train_dataloader: ", train_dataloader)
+print("eval_dataloader: ", eval_dataloader)
 
 trainer = Trainer(
     model,
     training_args,
     train_dataset=tokenized_datasets["train"],
     eval_dataset=tokenized_datasets["validation"],  # findet der nicht, warum findet er train_dataset?
+    #test_dataset=tokenized_datasets["test"], # das nennt der unerwartetes Argument
     data_collator=data_collator,
     tokenizer=tokenizer,
 )
 
 start_time = time.time()
+print("start_time: ", start_time)
 
-trainer.train()
+trainer.train()  #TODO, not working with T5: ValueError: not enough values to unpack (expected 2, got 1)
 
-predictions = trainer.predict(tokenized_datasets["validation"])
-print(predictions.predictions.shape, predictions.label_ids.shape)
+
+predictions_train = trainer.predict(tokenized_datasets["train"])
+
+#AttributeError: 'tuple' object has no attribute 'shape'
+#print("predictions_train.predictions.shape: ", predictions_train.predictions.shape)
+#print("predictions_train.label_ids.shape", predictions_train.label_ids.shape)
+
+predictions_eval = trainer.predict(tokenized_datasets["validation"])
+#print("predictions_eval.predictions.shape: ", predictions_eval.predictions.shape)
+#print("predictions_eval.label_ids.shape", predictions_eval.label_ids.shape)
+
+end_time = time.time()
+time_elapsed = end_time - start_time
+print("time_elapsed: ", time.strftime("%H:%M:%S", time.gmtime(time_elapsed)))    
 
 def compute_metrics(eval_preds):
-    metric = evaluate.load("glue", "mrpc")
+    metric = evaluate.load("json", data_files=data_files)
     logits, labels = eval_preds
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
 
 preds = np.argmax(predictions.predictions, axis=-1)
 
-metric = evaluate.load("glue", "mrpc")
+metric = evaluate.load("json", data_files=data_files)
 metric.compute(predictions=preds, references=predictions.label_ids)
 
 
 
 
-end_time = time.time()
-time_elapsed = end_time - start_time
-print("time_elapsed: ", time.strftime("%H:%M:%S", time.gmtime(time_elapsed)))        
+    
 
         
 
@@ -235,10 +283,6 @@ print("time_elapsed: ", time.strftime("%H:%M:%S", time.gmtime(time_elapsed)))
 
 
 
-sequences = [
-    "I've been waiting for a HuggingFace course my whole life.",
-    "This course is amazing!",
-]
 
 
 
